8e24dabf6b54c23199a3f35697f2e84971703150
valhallajazzy <valhallajazzy@mail.ru>
2023-11-03 19:02:28 +0300

    Refactor main.py: add exceptions

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import logging
 import requests
+from requests import HTTPError
+
+
 def main():
     payload = {"location": "", "lang": "ru", "M": "", "m": "", "n": "", "q": "", "T": ""}
     locations = ['Лондон', 'Череповец', 'svo']
     for location in locations:
-        response = requests.get(f'https://wttr.in/{location}', params=payload)
-        print(response.text)
+        try:
+            response = requests.get(f'https://wttr.in/{location}', params=payload)
+            response.raise_for_status()
+        except HTTPError:
+            logging.exception('Ошибка соединения с сервером')
+    print(response.text)
+
 
 if __name__ == '__main__':
     main()
-
