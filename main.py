9797b05519eddf4e2b9ec63659afe4c3881c92b2
valhallajazzy <valhallajazzy@mail.ru>
2023-11-04 12:24:31 +0300

    Refactor main.py: delete exception

--- main.py
+++ main.py
@@ -7,12 +7,9 @@ def main():
     payload = {"location": "", "lang": "ru", "M": "", "m": "", "n": "", "q": "", "T": ""}
     locations = ['Лондон', 'Череповец', 'svo']
     for location in locations:
-        try:
-            response = requests.get(f'https://wttr.in/{location}', params=payload)
-            response.raise_for_status()
-        except HTTPError:
-            logging.exception('Ошибка соединения с сервером')
-    print(response.text)
+        response = requests.get(f'https://wttr.in/{location}3463gg', params=payload)
+        response.raise_for_status()
+        print(response.text)
 
 
 if __name__ == '__main__':
